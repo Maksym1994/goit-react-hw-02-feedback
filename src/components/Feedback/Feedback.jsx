c26f54f120dc79ed9b7bad21d9519c950dfdaf6a
Maksym <tigron1994@gmail.com>
2021-09-07 15:40:04 +0200

    обновил данные приложения

---
+++ src/components/Feedback/Feedback.jsx
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import FeedbackOptions from 'components/FeedbackOption';
+import Statistic from 'components/Statistics';
+import Section from 'components/Section';
+import Notification from 'components/Notification';
+
+export class Feedback extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  handleIncrement = ({ target: { name } }) => {
+    this.setState(prevState => ({
+      [name]: prevState[name] + 1,
+    }));
+  };
+
+  totalFeedback() {
+    const { good, neutral, bad } = this.state;
+    return good + neutral + bad;
+  }
+
+  countPositiveFeedbackPercentage() {
+    const { good } = this.state;
+    return Math.round((good * 100) / this.totalFeedback());
+  }
+
+  render() {
+    const { good, neutral, bad } = this.state;
+    return (
+      <div>
+        <FeedbackOptions options={Object.keys(this.state)} onLeaveFeedback={this.handleIncrement} />
+        {this.totalFeedback() > 0 ? (
+          <Section title="Statistics">
+            <Statistic
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={this.totalFeedback()}
+              positivePercentage={this.countPositiveFeedbackPercentage()}
+            />
+          </Section>
+        ) : (
+          <Notification message="No feedback given" />
+        )}
+      </div>
+    );
+  }
+}
+
+export default Feedback;
---
+++ src/components/FeedbackOption/FeedbackOption.jsx
@@ -0,0 +1,16 @@
+import Section from 'components/Section';
+import s from 'components/FeedbackOption/FeedbackOption.module.css';
+
+const FeedbackOptions = ({ options, onLeaveFeedback }) => {
+  return (
+    <Section title="Feedback options">
+      {options.map(option => (
+        <button className={s.button} name={option} key={option} onClick={onLeaveFeedback}>
+          {option}
+        </button>
+      ))}
+    </Section>
+  );
+};
+
+export default FeedbackOptions;
